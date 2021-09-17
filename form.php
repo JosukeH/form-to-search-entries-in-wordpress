76984c0604b332ed7957a0718c3cedb8ab7c9867
JosukeH <41598663+JosukeH@users.noreply.github.com>
2021-09-17 11:15:35 -0500

    Create form.php

---
+++ form.php
@@ -0,0 +1,5 @@
+	
+<form id="searchform" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <input type="text" class="search-field top-nav-input" name="s" placeholder="Buscar..." value="">
+</form>
+							
